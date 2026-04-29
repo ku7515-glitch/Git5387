ed3fb1842fd2e964b60867e42e54db177d172dd0
ku7515-glitch <ku7515@gmail.com>
2026-04-29 16:35:56 +0900

    Add prime numbers

--- mynumber.c
+++ mynumber.c
@@ -4,13 +4,28 @@ int main(void) {
     for (int i = 1; i <= 100; i += 2) {
         printf("%d ", i);
     }
-
     printf("\n");
 
     for (int i = 2; i <= 100; i += 2) {
         printf("%d ", i);
     }
-
     printf("\n");
+
+    for (int i = 2; i <= 100; i++) {
+        int isPrime = 1;
+
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
     return 0;
 }
